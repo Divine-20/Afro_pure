eb7ee02527d3c7e43cecbf0724ee8efeaf8fdc5d
mwizerwa77 <mwizerwa77@gmail.com>
2023-10-30 15:35:49 +0200

    text

--- src/app/components/api-services/axios/index.ts
+++ src/app/components/api-services/axios/index.ts
@@ -1,4 +1,4 @@
 import Instance from 'axios'
 export const axios = Instance.create({
-    baseURL:"http://197.243.18.48/afropureapp/api/v1"
-}) 
+    baseURL: "http://197.243.18.48:8088/afropureapp/api/v1"
+})
