858a23959ed58d93d1170f1c4a067224ef2e09a2
Ingabire Divine <divineingabire69@gmail.com>
2023-10-31 15:38:31 +0200

    resolved conflicts

--- src/app/components/api-services/axios/index.ts
+++ src/app/components/api-services/axios/index.ts
@@ -1,16 +1,5 @@
 import Instance from 'axios'
 export const axios = Instance.create({
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 4e3252d (fixed stats issue)
-    baseURL: "http://197.243.18.48:8088/afropureapp/api/v1"
-})
-=======
-    baseURL:"http://10.10.66.228:8084/afropureapp/api/v1"
-=======
     baseURL:"http://197.243.18.48:8088/afropureapp/api/v1"
->>>>>>> 1968bd2 (updates(): Added logout)
 }) 
->>>>>>> 48ce607 (Fix: some fixes)
+
