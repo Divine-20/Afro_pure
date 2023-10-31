0804eaead346151d90063a8cd64cda97bb3b6dfe
Ingabire Divine <divineingabire69@gmail.com>
2023-10-31 15:24:12 +0200

    Initial commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-// import React, { useState } from "react";
 import React, { createContext, useState } from "react";
 import Image from "next/image";
 import AfroLogo from "../../../public/afropurelogo.png";
